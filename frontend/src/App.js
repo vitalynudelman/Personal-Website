83576c9f99d734ea0346c6c4c481efa0ff69e9da
MakeXCake <v.nudelman@gmail.com>
2024-03-26 22:34:29 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi, I'm [Your Name]</h1>
+        <p>I am a [your profession or expertise]. I enjoy [your interests or passions].</p>
+        <p>With a background in [your background or education], I specialize in [your skills or areas of focus].</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
