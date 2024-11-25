a59fd11eb8d3b2e42ca75d091a8fe1ff9036de21
Vitaly Nudelman <vitaly.nudelman@comm-it.com>
2024-11-25 11:30:55 +0200

    Updated contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,12 +5,8 @@ function Contact() {
   return (
     <div className="Contact">
       <h2>Contact Me</h2>
-      <p>Email: v.nudelman@gmail.com</p>
-      <p>Phone: +972 54 648 7047</p>
-      <p>GIT: https://github.com/makexcake</p>
 
       <div className="resume-section">
-        <h3>Resume</h3>
         <iframe src="resume/resume.pdf" width="100%" height="600px"></iframe>
         <a href="resume/resume.pdf" target="_blank" rel="noopener noreferrer" download>
           Download Resume
